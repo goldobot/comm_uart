07e04d50e9efa33f8e598b748e16e784bbd0da26
root <root@robot01>
2023-04-09 19:10:15 +0100

    * Add debug features (parsing of messages etc..)

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
 all:comm_uart
 
-comm_uart:
+comm_uart: main.cpp comm_serializer.cpp comm_deserializer.cpp crc.cpp zmq_sockets.cpp terminal.cpp
 	g++ main.cpp comm_serializer.cpp comm_deserializer.cpp crc.cpp zmq_sockets.cpp terminal.cpp -std=c++11 -lzmq -o comm_uart
 
+comm_uart_ftdi: main.cpp comm_serializer.cpp comm_deserializer.cpp crc.cpp zmq_sockets.cpp terminal.cpp
+	g++ -DDEBUG_FULL main.cpp comm_serializer.cpp comm_deserializer.cpp crc.cpp zmq_sockets.cpp terminal.cpp -std=c++11 -lzmq -o comm_uart_ftdi
+
 clean:
-	rm -f *.o comm_uart
+	rm -f *.o comm_uart comm_uart_ftdi
--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@
 #include "goldobot/comm_serializer.hpp"
 #include "goldobot/comm_deserializer.hpp"
 
+//#define DEBUG_FULL 1
+//#define DEBUG_HEARTBEAT 1
+//#define DEBUG_ADC 1
 
 int terminal_parse_speed(std::string speed_str);
 
@@ -36,7 +39,7 @@ static void sigint_handler(int sig)
 // pub port default 3001
 // sub port default 3002
 
-enum  optionIndex { UNKNOWN, HELP, PUB_PORT, SUB_PORT , BAUDRATE, DEVICE, COMM_ID};
+enum  optionIndex { UNKNOWN, HELP, PUB_PORT, SUB_PORT , BAUDRATE, DEVICE, COMM_ID, DEBUG};
 const option::Descriptor usage[] =
 {
  {UNKNOWN, 0, "", "",option::Arg::None, "Usage: comm_uart [Options] device_path\n\n"
@@ -47,6 +50,7 @@ const option::Descriptor usage[] =
  {BAUDRATE, 0,"b","baudrate",option::Arg::Optional, "  --baudrate, -b  \tuart baudrate." },
  {DEVICE, 0,"d","device",option::Arg::Optional, "  --device, -d  \tdevice." },
  {COMM_ID, 0,"i","id",option::Arg::Optional, "  --id, -i  \tcomm ide." },
+ {DEBUG, 0,"D","debug",option::Arg::None, "  --debug, -D  \tDEBUG." },
  {UNKNOWN, 0, "", "",option::Arg::Optional, "\nExamples:\n"
                                "  example --unknown -- --this_is_no_option\n"
                                "  example -unk --plus -ppp file1 file2\n" },
@@ -61,8 +65,289 @@ struct MessageHeader {
     int32_t time_nanoseconds;
 };
 
+static unsigned int old_heartbeat = 0;
+
+bool debug_dump_message(const char *prefix_s, struct MessageHeader *pheader, unsigned char *message_body, int body_len)
+{
+  uint16_t seq;
+  unsigned char *seq_offset = message_body + 4;
+  switch (pheader->message_type) {
+  case 0:    /* CommUartStats                  */
+    //printf ("  %s: CommUartStats\n", prefix_s);
+    break;
+  case 1:    /* CommUartPing                   */
+    //printf ("  %s: CommUartPing\n", prefix_s);
+    break;
+  case 2:    /* Heartbeat                      */
+  {
+    unsigned int my_heartbeat = *((unsigned int *)message_body);
+#ifdef DEBUG_HEARTBEAT
+    if ((my_heartbeat%1000)==0)
+      printf ("  %s: Heartbeat : %d  (msg_len=%d)\n", prefix_s, my_heartbeat, body_len);
+#endif
+
+    if (my_heartbeat!=(old_heartbeat+100))
+    {
+        printf ("   Heartbeat annomaly? : %d (msg_len=%d)\n", my_heartbeat, body_len);
+    }
+    old_heartbeat = my_heartbeat;
+    break;
+  }
+  case 3:     /* HeapStats                     */
+    //printf ("  %s: HeapStats\n", prefix_s);
+    break;
+  case 4:     /* Reset                         */
+    printf ("  %s: Reset\n", prefix_s);
+    break;
+  case 5:     /* GetNucleoFirmwareVersion      */
+    printf ("  %s: GetNucleoFirmwareVersion\n", prefix_s);
+    break;
+  case 6:     /* TaskStats                     */
+    printf ("  %s: TaskStats\n", prefix_s);
+    break;
+  case 7:     /* DbgTrace                      */
+    printf ("  %s: DbgTrace\n", prefix_s);
+    break;
+
+  case 10:    /* MatchTimer                    */
+    //printf ("  %s: MatchTimer\n", prefix_s);
+    break;
+  case 11:    /* MatchTimerStart               */
+    printf ("  %s: MatchTimerStart\n", prefix_s);
+    break;
+  case 12:    /* MatchEnd                      */
+    printf ("  %s: MatchEnd\n", prefix_s);
+    break;
+  case 13:    /* MatchTimerStop                */
+    printf ("  %s: MatchTimerStop\n", prefix_s);
+    break;
+
+  case 20:    /* DbgGpioGet                    */
+    printf ("  %s: DbgGpioGet\n", prefix_s);
+    break;
+  case 21:    /* DbgGpioGetStatus              */
+    printf ("  %s: DbgGpioGetStatus\n", prefix_s);
+    break;
+  case 22:    /* DbgGpioSet                    */
+    printf ("  %s: DbgGpioSet\n", prefix_s);
+    break;
+  case 23:    /* DbgPwmSet                     */
+    printf ("  %s: DbgPwmSet\n", prefix_s);
+    break;
+  case 24:    /* DbgSetStatus                  */
+    printf ("  %s: DbgSetStatus\n", prefix_s);
+    break;
+
+  case 29:    /* DbgGoldo                      */
+    printf ("  %s: DbgGoldo\n", prefix_s);
+    break;
+
+  case 30:    /* FpgaReadReg                   */
+    printf ("  %s: FpgaReadReg\n", prefix_s);
+    break;
+  case 31:    /* FpgaReadRegStatus             */
+    printf ("  %s: FpgaReadRegStatus\n", prefix_s);
+    break;
+  case 32:    /* FpgaWriteReg                  */
+    printf ("  %s: FpgaWriteReg\n", prefix_s);
+    break;
+  case 33:    /* SensorsState                  */
+    //printf ("  %s: SensorsState\n", prefix_s);
+    break;
+  case 34:    /* FpgaGpioState                 */
+    printf ("  %s: FpgaGpioState\n", prefix_s);
+    break;
+  case 35:    /* FpgaReadRegInternal           */
+    printf ("  %s: FpgaReadRegInternal\n", prefix_s);
+    break;
+  case 36: /* FpgaReadAdc                      */
+#ifdef DEBUG_ADC
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: FpgaReadAdc\n", prefix_s);
+#endif
+    break;
+  case 37: /* FpgaReadAdcOut                   */
+#ifdef DEBUG_ADC
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: FpgaReadAdcOut\n", prefix_s);
+#endif
+    break;
+
+  case 40: /* ServoAck                         */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: ServoAck seq=%d\n", prefix_s, seq);
+    break;
+  case 41: /* ServoMoveMultiple                */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: ServoMoveMultiple seq=%d\n", prefix_s, seq);
+    break;
+  case 42: /* ServoSetEnable                   */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: ServoSetEnable seq=%d\n", prefix_s, seq);
+    break;
+  case 43: /* ServoState                       */
+    printf ("  %s: ServoState\n", prefix_s);
+    break;
+  case 44: /* ServosMoving                     */
+    seq = *((uint16_t *)message_body);
+    //printf ("  %s: ServosMoving seq=%d\n", prefix_s, seq);
+    break;
+  case 45: /* ServoDisableAll                  */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: ServoDisableAll seq=%d\n", prefix_s, seq);
+    break;
+  case 46: /* ServoLiftsCmdRawObsolete         */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: ServoLiftsCmdRawObsolete seq=%d\n", prefix_s, seq);
+    break;
+  case 47: /* ServoGetState                    */
+    printf ("  %s: ServoGetState\n", prefix_s);
+    break;
+  case 48: /* ServoLiftDoHomingObsolete        */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: ServoLiftDoHomingObsolete seq=%d\n", prefix_s, seq);
+    break;
+  case 49: /* ServoSetMaxTorques               */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: ServoSetMaxTorques seq=%d\n", prefix_s, seq);
+    break;
+
+  case 50: /* ODriveRequestPacket              */
+    printf ("  %s: ODriveRequestPacket\n", prefix_s);
+    break;
+  case 51: /* ODriveResponsePacket             */
+    printf ("  %s: ODriveResponsePacket\n", prefix_s);
+    break;
+  case 52: /* ODriveTelemetry                  */
+    printf ("  %s: ODriveTelemetry\n", prefix_s);
+    break;
+  case 53: /* ODriveCommStats                  */
+    printf ("  %s: ODriveCommStats\n", prefix_s);
+    break;
+
+  case 60: /* DynamixelsRequest                */
+    printf ("  %s: DynamixelsRequest\n", prefix_s);
+    break;
+  case 61: /* DynamixelsResponse               */
+    printf ("  %s: DynamixelsResponse\n", prefix_s);
+    break;
+
+  case 100: /* PropulsionEnableSet             */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionEnableSet seq=%d\n", prefix_s, seq);
+    break;
+  case 101: /* PropulsionMotorsEnableSet       */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionMotorsEnableSet seq=%d\n", prefix_s, seq);
+    break;
+  case 104: /* PropulsionSetAccelerationLimits */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionSetAccelerationLimits seq=%d\n", prefix_s, seq);
+    break;
+  case 105: /* PropulsionSetPose               */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionSetPose seq=%d\n", prefix_s, seq);
+    break;
+  case 108: /* PropulsionClearError            */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionClearError seq=%d\n", prefix_s, seq);
+    break;
+  case 110: /* PropulsionSetSimulationMode     */
+    printf ("  %s: PropulsionSetSimulationMode\n", prefix_s);
+    break;
+  case 130: /* PropulsionCommandEvent          */
+    seq = *((uint16_t *)seq_offset);
+    printf ("\n----\n");
+    printf ("  %s: PropulsionCommandEvent seq=%d\n", prefix_s, seq);
+    printf ("\n----\n");
+    break;
+  case 131: /* PropulsionControllerEvent       */
+    printf ("\n----\n");
+    printf ("  %s: PropulsionControllerEvent\n", prefix_s);
+    printf ("    message_body:\n");
+    for (int i=0; i<32; i++)
+    {
+      printf (" %.2x", message_body[i]);
+    }
+    printf ("\n----\n");
+    break;
+  case 140: /* PropulsionExecuteTranslation    */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionExecuteTranslation seq=%d\n", prefix_s, seq);
+    break;
+  case 141: /* PropulsionExecuteMoveTo         */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionExecuteMoveTo seq=%d\n", prefix_s, seq);
+    break;
+  case 143: /* PropulsionExecutePointTo        */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionExecutePointTo seq=%d\n", prefix_s, seq);
+    break;
+  case 144: /* PropulsionExecuteFaceDirection  */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionExecuteFaceDirection seq=%d\n", prefix_s, seq);
+    break;
+  case 145: /* PropulsionExecuteTrajectory    */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionExecuteTrajectory seq=%d\n", prefix_s, seq);
+    break;
+  case 149: /* PropulsionExecuteReposition    */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionExecuteReposition seq=%d\n", prefix_s, seq);
+    break;
+  case 152: /* PropulsionODriveClearErrors     */
+    printf ("  %s: PropulsionODriveClearErrors\n", prefix_s);
+    break;
+  case 153: /* PropulsionExecutePointToBack    */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: PropulsionExecutePointToBack seq=%d\n", prefix_s, seq);
+    break;
+  case 200: /* RobotConfigLoadBegin            */
+    printf ("  %s: RobotConfigLoadBegin\n", prefix_s);
+    break;
+  case 201: /* RobotConfigLoadChunk            */
+    printf ("  %s: RobotConfigLoadChunk\n", prefix_s);
+    break;
+  case 202: /* RobotConfigLoadEnd              */
+    printf ("  %s: RobotConfigLoadEnd\n", prefix_s);
+    break;
+  case 230: /* LiftHomingDone                  */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: LiftHomingDone seq=%d\n", prefix_s, seq);
+    break;
+  case 231: /* LiftSetEnable                   */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: LiftSetEnable seq=%d\n", prefix_s, seq);
+    break;
+  case 232: /* LiftDoHoming                    */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: LiftDoHoming seq=%d\n", prefix_s, seq);
+    break;
+  case 233: /* LiftsCmdRaw                     */
+    seq = *((uint16_t *)message_body);
+    printf ("  %s: LiftsCmdRaw seq=%d\n", prefix_s, seq);
+    break;
+  default:
+#ifdef DEBUG_FULL
+    printf ("\n");
+    printf ("  %s: ?? (%d)\n", prefix_s, pheader->message_type);
+    printf ("    message_body:\n");
+    for (int i=0; i<32; i++)
+    {
+      printf (" %.2x", message_body[i]);
+    }
+    printf ("\n");
+    break;
+#endif
+    return false;
+  }
+  return true;
+}
+
 int main(int argc, char *argv[])
 {
+    bool debug_flag = false;
+
     argc-=(argc>0); argv+=(argc>0); // skip program name argv[0] if present
 
     option::Stats  stats(usage, argc, argv);
@@ -105,6 +390,12 @@ int main(int argc, char *argv[])
     if (options[COMM_ID])
     {
         comm_id = std::atoi(options[COMM_ID].arg);
+    };
+
+    if (options[DEBUG])
+    {
+        debug_flag = true;
+        printf ("DEBUG\n");
     };
 
     std::cout << "PUB port: " << pub_port << " SUB port: " << sub_port << "comm id: " << comm_id << "\n";
@@ -174,6 +465,11 @@ int main(int argc, char *argv[])
                 header.time_seconds = tv.tv_sec;
                 header.time_nanoseconds = tv.tv_nsec;
 
+                if (debug_flag)
+                {
+                    debug_dump_message("R<<<<N", &header, tmp_buffer, recv_message_size);
+                }
+
                 zmq_send(pub_socket, (const char*)(&header), sizeof(header), ZMQ_SNDMORE);
                 zmq_send(pub_socket, (const char*)(tmp_buffer), recv_message_size, 0);
             }
@@ -198,6 +494,33 @@ int main(int argc, char *argv[])
                 memcpy(&header, buff, sizeof(header));
                 serializer.push_message(header.message_type, buff+sizeof(header), bytes_read - sizeof(header));
             };
+
+            if (debug_flag)
+            {
+                unsigned char *message_body = buff + sizeof(MessageHeader);
+                uint16_t seq;
+#ifndef DEBUG_FULL
+                if ((header.message_type!=0) && (header.message_type!=1) && (header.message_type!=36))
+#endif
+                {
+                    printf ("\n");
+                    printf ("header.message_type=%d bytes_read=%d\n", header.message_type, (int)bytes_read);
+                    for (int i=0; i<32; i++)
+                    {
+                        printf (" %.2x", buff[i]);
+                    }
+                    printf ("\n");
+
+                    if (!debug_dump_message("R>>>>N", &header, message_body, bytes_read))
+                    {
+                        printf ("  R>>>>N: received UNKNOWN command code : %x (%d)\n",
+                                header.message_type, header.message_type);
+                    }
+
+                    printf ("\n");
+                }
+            }
+
         }
 
         // Send data to uart
